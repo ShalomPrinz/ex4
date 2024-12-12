611ec7aa51dff595f732d25042348f60727d6cfd
Shalom Prinz <shalom.prinz@gmail.com>
2024-12-12 12:54:11 +0200

    human pyramid done

--- ex4.c
+++ ex4.c
@@ -7,6 +7,9 @@ Assignment:
 #include <stdio.h>
 #include <string.h>
 
+// Cheerleaders human pyramid dimensions, e.g. 5 means 5x5 pyramid
+#define CL_DIMENSIONS 5
+
 // Task Defined Functions
 void task1_robot_paths();
 void task2_human_pyramid();
@@ -57,6 +60,9 @@ int main() {
     } while (task != 6);
 }
 
+/*
+ Calculates valid paths for the robot to get home (0, 0), Only left (column - 1) and down (row - 1) allowed
+*/
 int calculatePathsCount(int column, int row) {
     // Reached invalid column or invalid row values, path is not counted
     if (column < 0 || row < 0) return 0;
@@ -79,8 +85,54 @@ void task1_robot_paths() {
     printf("The total number of paths the robot can take to reach home is: %d\n", pathsCount);
 }
 
+/*
+ Validates given column is valid in the context of Pyramid
+*/
+int isValidPColumn(int column, int row) {
+    return column <= row;
+}
+
+/*
+ Calculates a given cheerleader at position (row, column) supported weight by the following formula:
+    cheerleader weight = her own weight + .5 * (the total weight of the two cheerleaders above her)
+*/
+float calculateSupportedWeight(float weights[CL_DIMENSIONS][CL_DIMENSIONS], int row, int column) {
+    // Reached invalid column or invalid row values, no cheerleader weight to return
+    if (column < 0 || row < 0 || !isValidPColumn(column, row)) return 0;
+
+    // Calculates the supported weight of the left parent of current cheerleader
+    float leftParentWeight = calculateSupportedWeight(weights, row - 1, column - 1);
+    // Calculates the supported weight of the right parent of current cheerleader
+    float rightParentWeight = calculateSupportedWeight(weights, row - 1, column);
+    // Calculates current cheerleader supported weight by given formula
+    return weights[row][column] + (float) .5 * (leftParentWeight + rightParentWeight);
+}
+
 void task2_human_pyramid() {
-    // Todo
+    printf("Please enter the weights of the cheerleaders:\n");
+
+    // Scan weights pyramid inputs
+    float weights[CL_DIMENSIONS][CL_DIMENSIONS];
+    for (int row = 0; row < CL_DIMENSIONS; row++) {
+        for (int column = 0; isValidPColumn(column, row); column++) {
+            scanf("%f", &weights[row][column]);
+
+            // Validate cheerleader weight is not a negative number
+            if (weights[row][column] < 0) {
+                printf("Negative weights are not supported.\n");
+                return;
+            }
+        }
+    }
+
+    // Calculate and print each cheerleader supported weight
+    printf("The total weight on each cheerleader is:\n");
+    for (int row = 0; row < CL_DIMENSIONS; row++) {
+        for (int column = 0; isValidPColumn(column, row); column++) {
+            printf("%.2f ", calculateSupportedWeight(weights, row, column));
+        }
+        printf("\n");
+    }
 }
 
 void task3_parenthesis_validator() {
